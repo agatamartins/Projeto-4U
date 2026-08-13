608be5d692491c8ab61826d92ab4d9a0f65fe8ff
agatamartins <149191964+agatamartins@users.noreply.github.com>
2026-08-13 17:47:30 -0300

    Configuração de deploy

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kc=r3kzbij*un6t!hno1=2%v^^6=^cejafyap^(q5mhrpf-&z0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -115,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL =  BASE_DIR / 'staticfiles/'
 
 # Configurações de Autenticação
 LOGIN_URL = '/login/'
